858ccb49fc6dea3da8a8302ea85fd037852dc7b3
AnkitaPriyadarshini-repos <ankitapriyadarshini8600@gmail.com>
2025-08-11 09:01:22 +0530

    Added all project files and structure

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use((req,res,next) => {
 // error handler
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "something went wrong!" } = err;
-    res.status(statusCode).render("error.js", { message });
+    res.status(statusCode).render("error", { err });
 });
 
 app.listen(8080, () => {
